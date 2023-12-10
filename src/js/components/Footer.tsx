a87fbd769155402bc40a3cf963289c14c35700ab
David Inga <david.inga@vizzuality.com>
2023-12-10 23:22:09 +0100

    dropdown for languages

--- src/js/components/Footer.tsx
+++ src/js/components/Footer.tsx
@@ -1,36 +1,57 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import LanguageDropdown from '../../js/components/header/LanguageDropdown';
 import { RootState } from '../../js/store/index';
 import { mapController } from '../../js/controllers/mapController';
 
-import '../../css/footer.scss';
+const appSettingsSelector = createSelector(
+  (state: RootState) => state.appSettings,
+  (appSettings) => ({
+    language: appSettings.language,
+    useAlternativeLanguage: appSettings.useAlternativeLanguage,
+    alternativeWebmap: appSettings.alternativeWebmap,
+    alternativeLanguage: appSettings.alternativeLanguage,
+  })
+);
 
 const Footer = (): JSX.Element => {
-  const footerLinks = useSelector(
-    (store: RootState) => store.appSettings.footerLinks
-  );
+  const { language, useAlternativeLanguage, alternativeWebmap, alternativeLanguage } = useSelector(appSettingsSelector);
+  const selectedLanguage = useSelector((store: RootState) => store.appState.selectedLanguage);
+  const footerLinks = useSelector((store: RootState) => store.appSettings.footerLinks);
   const attRef = useRef(null);
-  const scaleRef = useRef(null);
+
   useEffect(() => {
-    mapController.addScaleBar(scaleRef);
     mapController.addMapAttribution(attRef);
   }, []);
 
   const footerLinksItems = footerLinks.map((item, i: number) => {
     return (
-      <p key={i}>
+      <div key={i}>
         <a href={item.link} target="_blank" rel="noopener noreferrer">
           {item.label}
         </a>
-      </p>
+      </div>
     );
   });
 
   return (
-    <div className="footer-container">
-      <div className="scaleBar" ref={scaleRef}></div>
-      <div className="footer-links">{footerLinksItems}</div>
-      <div className="attribution" ref={attRef}></div>
+    <div className="absolute z-30 bg-white/40 left-0 bottom-0 w-full">
+      <div className="flex items-center justify-between space-x-4 p-4 text-xs">
+        {/* <div className="scaleBar" ref={scaleRef}></div> */}
+        <div>
+          {useAlternativeLanguage && alternativeWebmap && alternativeLanguage && (
+            <LanguageDropdown
+              language={language}
+              alternativeLanguage={alternativeLanguage}
+              selectedLanguage={selectedLanguage}
+            />
+          )}
+        </div>
+        <div className="">{footerLinksItems}</div>
+        <div className="" ref={attRef}></div>
+      </div>
     </div>
   );
 };
