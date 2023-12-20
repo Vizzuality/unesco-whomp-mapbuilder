d3ccd605c8e9c6b627ec9c1da89a48f2ca6b30d8
David Inga <david.inga@vizzuality.com>
2023-12-20 15:23:37 +0100

    added missing dot at the end of analysis text

--- configs/translations/leftPanel.translations.ts
+++ configs/translations/leftPanel.translations.ts
@@ -408,7 +408,7 @@ export const layersPanelTranslations = {
     tabTitle: '1. Visualize',
     layers: 'Layers',
     description:
-      'Select datasets from different categories that you wish to visualize on the map. Click on the layer to get additional information about the datasets. Metadata and tutorial is also available <a href="https://whc.unesco.org/uploads/activities/documents/activity-1142-19.pdf" target="_blank">here</a>',
+      'Select datasets from different categories that you wish to visualize on the map. Click on the layer to get additional information about the datasets. Metadata and tutorial is also available <a href="https://whc.unesco.org/uploads/activities/documents/activity-1142-19.pdf" target="_blank">here</a>.',
     selectAll: 'Select All',
     clearAll: 'Clear All',
     legend: 'Legend',
@@ -435,7 +435,7 @@ export const layersPanelTranslations = {
     tabTitle: '1. Visualizer',
     layers: 'Couches',
     description:
-      'Sélectionnez des ensembles de données de différentes catégories que vous souhaitez visualiser et ajouter à la carte. Les métadonnées et un tutoriel sont également disponible <a href="https://whc.unesco.org/uploads/activities/documents/activity-1142-19.pdf">ici</a>',
+      'Sélectionnez des ensembles de données de différentes catégories que vous souhaitez visualiser et ajouter à la carte. Les métadonnées et un tutoriel sont également disponible <a href="https://whc.unesco.org/uploads/activities/documents/activity-1142-19.pdf">ici</a>.',
     selectAll: 'Tout sélectionner',
     clearAll: 'Tout effacer',
     legend: 'Légende',
